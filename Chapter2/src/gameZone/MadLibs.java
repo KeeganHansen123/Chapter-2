22aea0ba379b1b195707431a7db8c8c2344cd5f4
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-09-05 09:28:56 -0500

    update to new folder

---
+++ Chapter2/src/gameZone/MadLibs.java
@@ -0,0 +1,45 @@
+package gameZone;
+
+import java.util.Scanner;
+
+public class MadLibs {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String color;
+		String wordEst;
+		String bodyPartPlural;
+		String anAnimal;
+		String aNoun;
+		String PluralNoun;
+		
+		int a, b, c;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a color >>");
+		color = input.nextLine();
+		System.out.print("Enter a word that ends in est >>");
+		wordEst = input.nextLine();
+		System.out.print("Enter a body part thats plural >>");
+		bodyPartPlural = input.nextLine();
+		System.out.print("Enter an Animal >>");
+		anAnimal = input.nextLine();
+		System.out.print("Enter a noun >>");
+		aNoun = input.nextLine();
+		System.out.print("Enter a Plural Noun >>");
+		PluralNoun = input.nextLine();
+		
+		System.out.print("The" + color + "Dragon is the" + wordEst + "in the world."
+				+ "");
+		
+		c = a - b;
+		
+		System.out.print("Enter a number>> ");
+		a = input.nextInt();
+		System.out.print("Enter another number>> ");
+		b = input.nextInt();
+	}
+
+}
